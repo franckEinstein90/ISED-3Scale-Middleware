6cf257bbc66ca961cdc520438037976ecd2fbb82
Franck <franckbinard@gmail.com>
2019-10-02 17:21:26 -0400

    first API call done

--- src/userInfo.js
+++ src/userInfo.js
@@ -7,12 +7,59 @@ const async = require('async');
 
 const userInfo = (function(){
 
-	let tenants, userURL, getClientIDForTenant;
+	let tenants, userURL, 
+		getClientIDsFromEmail, getClientIDForTenant, getTenantSubscriptionKeysForUser; 
 
 	tenants = [] 
 
 	userURL  = function(){
 
+	}
+
+
+	getClientIDsFromEmail = function(email){
+		let processPayload = function(payload){
+			let userID = payload.account.id
+			return userID;
+		}
+		let processSubscriptionInfoPayload = function(payload){
+			console.log(payload)
+		}
+		async.each(tenants, function(tenant, callback){
+			 getClientIDForTenant(tenant, email)
+				.then(processPayload)
+				.then(function(userid) {
+					let appPromise = getTenantSubscriptionKeysForUser(userid, tenant)
+					appPromise.then(processSubscriptionInfoPayload)
+				})
+				.catch(error => console.log("rejected" + error))
+		}, function(err){
+			if(err){
+				console.log(err)
+			}
+		})
+	}
+
+	getTenantSubscriptionKeysForUser = function(userid, tenant){
+		let baseURL = `https://${tenant.value.admin_domain}/admin/api/accounts/` 
+		let apiCall = `${userid}/applications.json?access_token=${tenant.value.access_token}`
+	
+		let tenantCallPromise = new Promise(function(resolve, reject) {
+				let request = unirest("GET", baseURL + apiCall);
+				request.headers({
+					'Content-Type': 'text/html',
+					'Accept':'application/json', 
+					'User-Agent':'ISED Middleware'
+				})	
+				request.end(function(response){
+					if(response.error || response.code !== 200){
+						reject(response.error)
+					}else{
+						resolve(response.body)
+					}
+				})
+			})	
+			return tenantCallPromise
 	}
 
 	getClientIDForTenant = function(tenant, email){
@@ -22,17 +69,25 @@ const userInfo = (function(){
 
 		return new Promise((resolve, reject)=>{
 
-			let req = unirest("GET", baseURL + apiCall);
-				
-			req.end(function (res) {
-				if (res.error) reject(res.error)
-				resolve(res.body);
+			let request = unirest("GET", baseURL + apiCall);
+			request.headers({
+				'Content-Type': 'text/html',
+				'Accept':'application/json', 
+				'User-Agent':'ISED Middleware'
+			})	
+			request.end(function (response) {
+				if (response.error || response.code !== 200) {
+					reject(response.error)
+				}
+				else{
+					resolve(response.body);
+				}
 			})
 		})
 	}
 
 	return{
-		
+
 		languages: {
 			francais: 2,
 			english: 1
@@ -44,18 +99,12 @@ const userInfo = (function(){
 		}, 
 
 		getInfo: function({email, language}){
+			let associatedIDs = getClientIDsFromEmail(email)
+			console.log(associatedIDs)
 			//validateEmail(email)
 			//assert language == fr or en
-			let ids = []
-			let processPayload = function(payload){
-				let userID = payload.account.id
-				ids.push(userID)	
-			}
-			async.each(tenants, function(tenant, callback){
-				let idPromise = getClientIDForTenant(tenant, email)
-				idPromise.then(processPayload).catch(console.log("rejected"))
-			})
 		}
+		
 	}
 })()
 
