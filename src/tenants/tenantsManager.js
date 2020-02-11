a826255b8e5cd76479dbe15da6c561532f74caa0
Franck Binard <franckbinard@gmail.com>
2020-02-11 16:59:59 -0500

    Update tenantsManager.js

--- src/tenants/tenantsManager.js
+++ src/tenants/tenantsManager.js
@@ -1,22 +1,71 @@
+/***********************************************************************************
+ * Franck Binard, ISED
+ * Canadian Gov. API Store middleware
+ * -------------------------------------
+ *  Module tenantsManager / server side
+ *
+ *  - manages a store of tenants
+ *  - replies to API requests userInfo.json and apiInfo.json
+ *  - updates the tenant and service information on a schedule
+ **********************************************************************************/
 "use strict"
 
+/**********************************************************************************/
+const moment = require('moment')
+const errors = require('@src/errors').errors
+/**********************************************************************************/
 const Tenant = require('@src/responses').tenants.Tenant
 
-const tenantsManager = (function(){
+const tenantsManager = (function() {
 
     let _tenants = []
+    let _updateRegister = new Map()
 
     return {
-        configure: function( appData ){
+        tenants: _ => _tenants,
+
+        configure: function(apiCan) {
             return new Promise((resolve, reject) => {
-                appData.data.tenants.forEach( (tenantInfo, tenantName) => {
+                apiCan.data.tenants.forEach((tenantInfo, tenantName) => {
                     if (tenantInfo.visible) {
-                        _tenants.push ( new Tenant( tenantInfo, appData.env ))
-                        appData.say(`Added ${tenantName} to tenant register`)
+                        _tenants.push(new Tenant(tenantInfo, apiCan.env))
+                        apiCan.say(`Added ${tenantName} to tenant register`)
                     }
                 })
-                resolve( appData )
+                return resolve(apiCan)
             })
+        },
+
+        updateTenantInformation: async function(listToUpdate = null) {
+            /********************************************************
+             * Called by cron job, updates all tenant information in 
+             * memory. If listToUpdate specified, tenant manager 
+             * only updates specified tenants
+             * *****************************************************/
+            let tenantsToUpdate = null
+            if (listToUpdate) {
+                tenantsToUpdate = listToUpdate.map(tName => _tenants.find(t => t.name === tName))
+            } else {
+                tenantsToUpdate = /*all*/ _tenants
+            }
+
+            let registerUpdatedTenants = tenantsUpdateReport => {
+                tenantsUpdateReport.forEach(updateReport => {
+                    let currentTime = moment()
+                    updateReport.endUpdateTime = currentTime
+                    if (updateReport.updateSuccess === errors.codes.Ok) {
+                        _updateRegister.set(updateReport.tenantName, currentTime)
+                    }
+                })
+                return tenantsUpdateReport
+            }
+
+            return Promise.all(tenantsToUpdate.map(tenant => {
+                    return tenant.updateApiInfo()
+                }))
+                .then(updateReports => {
+                    return registerUpdatedTenants(updateReports)
+                })
         }
     }
 })()
